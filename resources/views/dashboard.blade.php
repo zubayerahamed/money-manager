61835b5111a36f1ed79cfe61106fafe69ef43c10
Zubayer Ahamed <zubayerahamed.freelancer@gmail.com>
2023-05-09 17:55:43 +0600

    09-05-2023

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,10 +46,73 @@
                             @foreach ($monthWiseGroup as $key => $val)
                                 @if ($val['month'] == date('m'))
                                     <div class="row mt-2">
-                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100"><h5 class="m-0">Current Balance <br/> {{ $currentBalance }} TK</h5></button></div>
-                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100"><h5 class="m-0 text-success">Income <br/> {{ $val['income'] }} TK</h5></button></div>
-                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100"><h5 class="m-0 text-danger">Expense <br/> {{ $val['expense'] + $val['trancharge'] }} TK</h5></button></div>
-                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100"><h5 class="m-0 text-primary">Saving <br/> {{ $val['saving'] }} TK</h5></button></div>
+                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100">
+                                                <h5 class="m-0">Current Balance <br /> {{ $currentBalance }} TK</h5>
+                                            </button></div>
+                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100">
+                                                <h5 class="m-0 text-success">Income <br /> {{ $val['income'] }} TK</h5>
+                                            </button></div>
+                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100">
+                                                <h5 class="m-0 text-danger">Expense <br /> {{ $val['expense'] + $val['trancharge'] }} TK</h5>
+                                            </button></div>
+                                        <div class="col-md-3 mt-2"><button class="btn btn-light btn-sm w-100">
+                                                <h5 class="m-0 text-primary">Saving <br /> {{ $val['saving'] }} TK</h5>
+                                            </button></div>
+                                    </div>
+
+
+                                    <div class="row">
+                                        <div class="col-sm-6 col-xl-3">
+                                            <div class="card card-body bg-primary text-white">
+                                                <div class="d-flex align-items-center">
+                                                    <div class="flex-fill">
+                                                        <h4 class="mb-0">54,390</h4>
+                                                        total comments
+                                                    </div>
+                
+                                                    <i class="ph-chats ph-2x opacity-75 ms-3"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                
+                                        <div class="col-sm-6 col-xl-3">
+                                            <div class="card card-body bg-danger text-white">
+                                                <div class="d-flex align-items-center">
+                                                    <div class="flex-fill">
+                                                        <h4 class="mb-0">389,438</h4>
+                                                        total orders
+                                                    </div>
+                
+                                                    <i class="ph-package ph-2x opacity-75 ms-3"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                
+                                        <div class="col-sm-6 col-xl-3">
+                                            <div class="card card-body bg-success text-white">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="ph-hand-pointing ph-2x opacity-75 me-3"></i>
+                
+                                                    <div class="flex-fill text-end">
+                                                        <h4 class="mb-0">652,549</h4>
+                                                        total clicks
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                
+                                        <div class="col-sm-6 col-xl-3">
+                                            <div class="card card-body bg-indigo text-white">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="ph-users-three ph-2x opacity-75 me-3"></i>
+                
+                                                    <div class="flex-fill text-end">
+                                                        <h4 class="mb-0">245,382</h4>
+                                                        total visits
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 @endif
                             @endforeach
